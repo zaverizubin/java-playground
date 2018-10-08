eacfb7e5803d91f74fcd1410800d4b1da0678c67
z.zaveri <zubin.zaveri@focalworks.in>
2018-10-08 20:13:42 +0530

    wip

---
+++ spring-example/src/com/javapapers/spring/mvc/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.javapapers.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/")
+	public String hello() {
+		return "hello";
+	}
+
+	@RequestMapping(value = "/hi", method = RequestMethod.GET)
+	public String hi(@RequestParam("name") String name, Model model) {
+		String message = "Hi " + name + "!";
+		model.addAttribute("message", message);
+		return "hi";
+	}
+
+}
